76c61d9dc0b1a7dfe5962f82332b3c3a2780b23f
ringohub <ringohub@gmail.com>
2015-06-19 14:35:58 +0900

    Add run usage

--- src/main/g8/$name$.sh
+++ src/main/g8/$name$.sh
@@ -4,10 +4,19 @@ source ./modules/cmdinfo.sh
 source ./modules/cecho.sh
 source ./modules/usage.sh
 
+info "Infomation Message\n"
+error "Error Message\n"
+warn "Warning Message\n"
+success "Success!!\n\n"
+
+usage
+
 while [[ $# -gt 0 ]]; do
   case ${1} in
     --verbose|-V)
-      VERBOSE_MODE=true;;
+      VERBOSE_MODE=true
+      set -x
+      ;;
     --help|-h)
       usage;;
     *)
